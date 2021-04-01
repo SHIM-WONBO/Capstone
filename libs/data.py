91fda9be1f961cf0a8103b88ca99e0282f7a424d
SHIM-WONBO <59244722+SHIM-WONBO@users.noreply.github.com>
2021-04-01 15:52:27 +0900

    DB lib

--- libs/data.py
+++ libs/data.py
@@ -2,8 +2,8 @@ import pymysql.cursors
 
 class db:
     # connection 정보
-        
-    def num(self, inx, a, b, c, d, e, f):
+    
+    def num_w(self, a, b, c, d, e, f):
         conn = pymysql.connect(
         host = '192.168.0.32', # host name
         user = 'root', # user name
@@ -12,13 +12,50 @@ class db:
         charset = 'utf8'
         )
         with conn.cursor() as cursor:
-            sql = 'INSERT INTO test_num (INX, hadong, jidong, ingyedong, umandong, yeonmudong, iuidong) VALUES (%s, %s, %s, %s, %s, %s, %s)'
-            cursor.execute(sql, (inx, a, b, c, d, e, f))
+            sql = 'INSERT INTO test_num (yeonmudong, umandong, iuidong, jidong, ingyedong, hadong) VALUES (%s, %s, %s, %s, %s, %s)'
+            cursor.execute(sql, (a, b, c, d, e, f))
         conn.commit()
 
         conn.close()
 
-    def area(self, inx, bar_data):
+    def num_r(self):
+        try:
+            conn = pymysql.connect(
+            host = '192.168.0.32', # host name
+            user = 'root', # user name
+            password = '2222', # password
+            db = 'ddb', # db name
+            charset = 'utf8'
+            )
+            
+            with conn.cursor() as cursor:    
+                sql = "select * from test_num"
+                cursor.execute(sql)
+            
+            rows = cursor.fetchall()
+            #print(rows[-1][0], rows[-1][1], rows[-1][2], rows[-1][3], rows[-1][4], rows[-1][5], rows[-1][6])
+            return rows[-1][1], rows[-1][2], rows[-1][3], rows[-1][4], rows[-1][5], rows[-1][6]
+        except IndexError:
+            db.num_w(self,0,0,0,0,0,0)
+            conn = pymysql.connect(
+            host = '192.168.0.32', # host name
+            user = 'root', # user name
+            password = '2222', # password
+            db = 'ddb', # db name
+            charset = 'utf8'
+            )
+            
+            with conn.cursor() as cursor:    
+                sql = "select * from test_num"
+                cursor.execute(sql)
+            
+            rows = cursor.fetchall()
+            #print(rows[-1][0], rows[-1][1], rows[-1][2], rows[-1][3], rows[-1][4], rows[-1][5], rows[-1][6])
+            return rows[-1][1], rows[-1][2], rows[-1][3], rows[-1][4], rows[-1][5], rows[-1][6]
+        conn.commit()
+        conn.close()
+
+    def area(self, bar_data):
         conn = pymysql.connect(
         host = '192.168.0.32', # host name
         user = 'root', # user name
@@ -27,8 +64,13 @@ class db:
         charset = 'utf8'
         )    
         with conn.cursor() as cursor:
-            sql = 'INSERT INTO test_area (INX, AREA) VALUES (%s, %s)'
-            cursor.execute(sql, (inx, bar_data))
+            sql = 'INSERT INTO test_area (AREA) VALUES (%s)'
+            cursor.execute(sql, (bar_data))
         conn.commit()
 
         conn.close()
+
+if __name__ == "__main__":
+    import sys
+    db = db()
+    db.num_r()
